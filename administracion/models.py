ba6cfaf9b6920acaf57494e707ed23c900b7f854
Jorge Eduardo  Antilef <jorge.antilef@dafitis.cl>
2020-11-04 23:50:25 -0300

    primer commit, estructuta de carpetas creadas, tabla visitas lista,

---
+++ administracion/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+from django.urls import reverse
+
+
+class visits (models.Model):
+    rut = models.CharField(max_length=15)
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    tower = models.CharField(max_length=1)
+    department = models.CharField(max_length=10)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.name} ({self.last_name})'
