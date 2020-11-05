a6c3f80c8ad691f779ab810185415686d3789500
Jorge Eduardo  Antilef <jorge.antilef@dafitis.cl>
2020-11-05 17:08:36 -0300

    CRUD de administrador listo

--- administracion/models.py
+++ administracion/models.py
@@ -13,3 +13,10 @@ class visits (models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.name} ({self.last_name})'
+
+
+class salon(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.CharField(max_length=15)
+    fecha = models.DateField()
+    salon = models.CharField(max_length=15)
